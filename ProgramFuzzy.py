c35ec4aafe4251860a80be2efef05de5853ea3d7
Yasmin <yasminmufidah32@gmail.com>
2021-12-11 02:45:07 +0700

    menambahkan perhitungan myu

--- ProgramFuzzy.py
+++ ProgramFuzzy.py
@@ -9,112 +9,111 @@ root.configure(bg=defaultcolor)
 def Inference():
 	inp1 = inputtxt1.get(1.0, "end-1c")
 	inp1 = int(inp1)
+	var1 = [0,0,0,0,0,0,0,1,1,1,1,1,1,1,2]
+	myu_1 = []
+	for i in var1:
+		if i == 0:
+			if inp1 <= 20:
+				myu1 = 1
+			elif inp1 > 20 and inp1 < 25:
+				myu1 = (25 - inp1)/(25-20)
+			elif inp1 >= 25:
+				myu1 = 0
+			Output.insert(END,str(myu1))
+		elif i == 1:
+			if inp1 <= 20:
+				myu1 = 1
+			elif inp1 > 20 and inp1 <= 37.5:
+				myu1 = (inp1-20)/(37.5-20)
+			elif inp1 > 37.5 and inp1 < 55:
+				myu1 = (55-inp1)/(55-37.5)
+			elif inp1 >= 55:
+				myu1 = 0
+			Output.insert(END,str(myu1))
+		elif i == 2:
+			if inp1 <= 50:
+				myu1 = 0
+			elif inp1 > 50 and inp1 < 85:
+				myu1 = (inp1-50)/(85-50)
+			elif inp1 >= 85:
+				myu1 = 1
+			Output.insert(END,str(myu1))
+		myu_1.append(myu1)
 
-	
-	if inp1 <= 20:
-		if inp1 <= 20:
-			myu1 = 1
-		elif inp1 > 20 and inp1 < 25:
-			myu1 = (25 - inp1)/(25-20)
-		elif inp1 >= 25:
-			myu1 = 0
-		Output.insert(END,'Ringan')
-	elif inp1 > 20 and inp1 < 55 :
-		if inp1 <= 20:
-			myu1 = 1
-		elif inp1 > 20 and inp1 <= 37.5:
-			myu1 = (inp1-20)/(37.5-20)
-		elif inp1 > 37.5 and inp1 < 55:
-			myu1 = (55-inp1)/(55-37.5)
-		elif inp1 >= 55:
-			myu1 = 0
-		Output.insert(END,'Sedang')
-	elif inp1 > 50 and inp1 < 85 :
-		if inp1 <= 50:
-			myu1 = 0
-		elif inp1 > 50 and inp1 < 85:
-			myu1 = (inp1-50)/(85-50)
-		elif inp1 >= 85:
-			myu1 = 1
-		Output.insert(END,'Kencang')
-
-	
 	inp2 = inputtxt2.get(1.0, "end-1c")
-	inp2 = int(inp2)
-
-	if inp2 > 0.1 and inp2 < 1.5:
-		Kategori = 'Rendah'
-		if inp2 <= 1:
-			myu2 = 1
-		elif inp2 > 1 and inp2 < 1.7:
-			myu2 = ( 1.5 - inp2 ) / ( 1.5 - 1 )
-		elif inp2 >= 1.5:
+	inp2 = float(inp2)
+	var2 = [0,0,0,1,1,1,2,0,0,0,1,1,1,2,3]
+	Output3.insert(END, str(inp2))
+	myu_2 = []
+	for i in var2:
+		if i == 0:
+			if inp2 <= 1:
+				myu2 = 1
+			elif inp2 > 1 and inp2 < 1.7:
+				myu2 = ( 1.5 - inp2 ) / ( 1.5 - 1 )
+			elif inp2 >= 1.5:
+				myu2 = 0
+			Output1.insert(END,str(myu2))
+		elif i == 1:
+			if inp2 <= 1:
+				myu2 = 1
+			elif inp2 > 1 and inp2 <=1.75:
+				myu2 = ( inp2 - 1 ) / ( 1.75 - 1)
+			elif inp2  > 1.5 and inp2  < 2.5:
+				myu2 = ( 2.5 - inp2 ) / ( 2.5 - 1.5 )
+			elif inp2 >= 2.5:
+				myu2 = 0
+			Output1.insert(END,str(myu2))
+		elif i == 2:
+			if inp2 <= 2:
+				myu2 = 0
+			elif inp2 > 2 and inp2 < 5:
+				myu2 = ( inp2 - 2 )/( 5 - 2 )
+			elif inp2 >= 5:
+				myu2 = 1
+			Output1.insert(END,str(myu2))
+		elif i == 3 :
 			myu2 = 0
-
-	if inp2 > 1 and inp2 < 2.5:
-		Kategori = 'Sedang'
-		if inp2 <= 1:
-			myu2 = 1
-		elif inp2 > 1 and inp2 <=1.75:
-			myu2 = ( inp2 - 1 ) / ( 1.75 - 1)
-		elif inp2  > 1.5 and inp2  < 2.5:
-			myu2 = ( 2.5 - inp2 ) / ( 2.5 - 1.5 )
-		elif inp2 >= 2.5:
-			myu3 = 0
-
-	if inp2 > 2 and inp2 < 5:
-		Kategori = ' Tinggi'
-		if inp2 <= 2:
-			myu3 = 0
-		elif inp2 > 2 and inp2 < 5:
-			myu3 = ( inp2 - 2 ) - ( 5 - 2 )
-		elif inp2 >= 5:
-			myu3 = 1
+		myu_2.append(myu2)
 
 	inp3 = inputtxt3.get(1.0, "end-1c")
 	inp3 = int(inp3)
-
-	if inp3 >= 0 and inp3 <= 20 :
-		if inp3 <= 15:
-			myu3 = 1
-		elif inp3 > 15 and inp3 < 20:
-			myu3 = (20 - inp3)/(20-15)
-		elif inp3 >= 20:
+	var3 = [0,1,2,0,1,2,3,0,1,2,0,1,2,3,3]
+	myu_3 = []
+	for i in var3:
+		if i == 0 :
+			if inp3 <= 15:
+				myu3 = 1
+			elif inp3 > 15 and inp3 < 20:
+				myu3 = (20 - inp3)/(20-15)
+			elif inp3 >= 20:
+				myu3 = 0
+			Output2.insert(END,str(myu3))
+		elif i == 1:
+			if inp3 <= 15:
+				myu3 = 0
+			elif inp3 > 15 and inp3 <= 32.5:
+				myu3 = (inp3-15)/(32.5-15)
+			elif inp3 > 32.5 and inp3 < 50:
+				myu3 = (50-inp3)/(50-32.5)
+			elif inp3 >= 50:
+				myu3 = 0
+			Output2.insert(END,str(myu3))
+		elif i == 2 :
+			if inp3 <= 45:
+				myu3 = 0
+			elif inp3 > 45 and inp3 < 100:
+				myu3 = (inp3-45)/(100-45)
+			elif inp3 >= 100:
+				myu3 = 1
+			Output2.insert(END,str(myu3))
+		elif i == 3 :
 			myu3 = 0
-		Output2.insert(END,'Ringan')
-	elif inp3 >= 15 and inp3 <= 50 :
-		if inp3 <= 15:
-			myu3 = 0
-		elif inp3 > 15 and inp3 <= 32.5:
-			myu3 = (inp3-15)/(32.5-15)
-		elif inp3 > 32.5 and inp3 < 50:
-			myu3 = (50-inp3)/(50-32.5)
-		elif inp3 >= 50:
-			myu3 = 0
-		Output2.insert(END,'Sedang')
-	elif inp3 >=45 and inp3 <= 100 :
-		if inp3 <= 45:
-			myu3 = 0
-		elif inp3 > 45 and inp3 < 100:
-			myu3 = (inp3-45)/(100-45)
-		elif inp3 >= 100:
-			myu3 = 1
-		Output2.insert(END,'Lebat')
+		myu_3.append(myu3)
 
-	alpha = min(myu1, myu2, myu3)
-	Output3.insert(END, alpha)
+	# alpha = min(myu1, myu2, myu3)
+	# Output3.insert(END, alpha)
 
-
-def rule1(var1='ringan', var2 ='rendah', var3='ringan', out='aman'):
-	
-		return
-
-		# def kecepatanAngin():
-		# 	return 
-		# def KetinggianGelombang():
-		# 	return
-		# def CurahHujan():
-		# 	return
 l1 = Label(text = "Input Kecepatan Angin (km/jam)")
 l1.configure(bg=defaultcolor, font='helvetica')
 inputtxt1 = Text(root, height = 2,
